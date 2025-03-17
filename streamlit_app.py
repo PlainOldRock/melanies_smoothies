78fe6abb6435d801656b1a31fe9bea365cb93a2c
PlainOldRock <77250381+PlainOldRock@users.noreply.github.com>
2025-03-17 14:58:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,57 +5,59 @@ import requests
 import pandas as pd
 
 st.title("GITHUB Integration")
-st.write("This wa sbushed in github and updated the snowflake repo")
-"""helpful_links = [
-    "https://docs.streamlit.io",
-    "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
-    "https://github.com/Snowflake-Labs/snowflake-demo-streamlit",
-    "https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake"
-]
+st.write("This was pushed in github and updated the snowflake repo")
 
-# Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
-st.write("Choose the fruits you want in your custom Smoothie!")
 
-name_on_order = st.text_input('Name on Smoothie:')
-st.write('The name on your Smoothie will be:', name_on_order)
+# """helpful_links = [
+#     "https://docs.streamlit.io",
+#     "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
+#     "https://github.com/Snowflake-Labs/snowflake-demo-streamlit",
+#     "https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake"
+# ]
 
-cnx = st.connection("snowflake")
-session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+# # Write directly to the app
+# st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+# st.write("Choose the fruits you want in your custom Smoothie!")
 
-ingredients_list = st.multiselect(
-    'Choose up to 5 ingedients:'
-    , my_dataframe,
-    max_selections=5
-)
+# name_on_order = st.text_input('Name on Smoothie:')
+# st.write('The name on your Smoothie will be:', name_on_order)
 
-pd_df = my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop()
+# cnx = st.connection("snowflake")
+# session = cnx.session()
+# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+# #st.dataframe(data=my_dataframe, use_container_width=True)
 
-if ingredients_list:
-    ingredients_string = ''
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+# ingredients_list = st.multiselect(
+#     'Choose up to 5 ingedients:'
+#     , my_dataframe,
+#     max_selections=5
+# )
 
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        #st.write(f'The search value for {fruit_chosen} is {search_on}.')
+# pd_df = my_dataframe.to_pandas()
+# #st.dataframe(pd_df)
+# #st.stop()
+
+# if ingredients_list:
+#     ingredients_string = ''
+#     for fruit_chosen in ingredients_list:
+#         ingredients_string += fruit_chosen + ' '
+
+#         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+#         #st.write(f'The search value for {fruit_chosen} is {search_on}.')
         
-        st.subheader(fruit_chosen + " Nutrition Information")
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
-        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
-    #st.write(ingredients_string)
+#         st.subheader(fruit_chosen + " Nutrition Information")
+#         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
+#         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+#     #st.write(ingredients_string)
     
-    my_insert_stmt = f"insert into smoothies.public.orders(ingredients,name_on_order) values('{ingredients_string}','{name_on_order}')"
+#     my_insert_stmt = f"insert into smoothies.public.orders(ingredients,name_on_order) values('{ingredients_string}','{name_on_order}')"
 
-    time_to_insert = st.button("Submit Order")
-    #st.write(my_insert_stmt)
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
+#     time_to_insert = st.button("Submit Order")
+#     #st.write(my_insert_stmt)
+#     if time_to_insert:
+#         session.sql(my_insert_stmt).collect()
+#         st.success('Your Smoothie is ordered!', icon="✅")
 
-# New section to display smoothiefroot nutrition information"""
+# # New section to display smoothiefroot nutrition information"""
 
 
